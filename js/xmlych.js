ef5fed0b548fd6b5ebc9d9bbc9bb8865e6d5aa1a
Aaron <115772088@qq.com>
2020-06-16 14:49:28 +0800

    f

--- js/xmlych.js
+++ js/xmlych.js
@@ -4,8 +4,8 @@ var obj = JSON.parse(body);
 
 const vip = '/vip/query';
 const account = '/account/queryByParentId';
-
-if (url.indexOf(vip) != -1 || url.indexOf(time) != -1) {
+$notify(url,"d","e")
+if (url.indexOf(vip) >=0 || url.indexOf(account) >=0) {
     obj.data.parent["expiryTime"]="2088-12-32 23:59:59";
     obj.data.parent["vipFlag"]= 1;
     obj.data.parent["vipRank"]= 1;
