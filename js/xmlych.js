f68f05037f0532a0fcea5b081f7bba529e871f8d
Aaron <115772088@qq.com>
2020-06-16 11:54:29 +0800

    add xmly

---
+++ js/xmlych.js
@@ -0,0 +1,16 @@
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/vip/query';
+const account = '/account/queryByParentId';
+
+if (url.indexOf(vip) != -1 || url.indexOf(time) != -1) {
+    obj.data.parent["expiryTime"]="2088-12-32 23:59:59";
+    obj.data.parent["vipFlag"]= 1;
+    obj.data.parent["vipRank"]= 1;
+    obj.data.parent["vipType"]= 1;
+	body = JSON.stringify(obj);
+}
+
+$done({body});
